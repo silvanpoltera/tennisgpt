14e8f5e946d4a75b5b93c78bf47b62b65e35084f
Silvan Poltera <76483589+silvanpoltera@users.noreply.github.com>
2025-04-24 15:38:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import os
 from dotenv import load_dotenv
 from langchain_community.embeddings import OpenAIEmbeddings
-from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.chat_models import ChatOpenAI
@@ -21,7 +21,8 @@ docs = splitter.split_documents(documents)
 
 # Vektorstore
 embedding = OpenAIEmbeddings()
-vectorstore = FAISS.from_documents(docs, embedding)
+vectorstore = Chroma.from_documents(docs, embedding)
+
 
 # Chain
 chain = load_qa_chain(ChatOpenAI(model="gpt-4", temperature=0), chain_type="stuff")
